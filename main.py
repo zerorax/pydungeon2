338cdaefdb5ebf69f2dd0a515ac1dea4feb3cef8
Will O <zerorax@mail.ru>
2020-01-28 06:56:00 -0500

    minor additions to initial framework

--- main.py
+++ main.py
@@ -13,14 +13,23 @@ STATE_INTRO = 4
 STATE_INGAME = 5
 
 TICK_SECONDS = 1.0
+START_RNUM = 1
 
+class Character:
+    def __init__(self, name, shortdesc, longdesc, location=START_RNUM, gold=0, inventory=[]):
+        self.name = name
+        self.shortdest = shortdesc
+        self.longdest = longdesc
+        self.gold = gold
+        self.inventory = inventory
+        self.location = gamestate.roomdict[location]
 
 class Command:
-    def __init__(self, name, min_argc, function,help):
+    def __init__(self, name, min_argc, function, docs):
         self.name = name
         self.min_argc = min_argc
         self.function = function
-        self.help = help
+        self.docs = docs
 
     def execute(self,character, argc, argv):
         self.function(character, argc, argv)
@@ -35,8 +44,12 @@ class GameState:
         self.npclist = []
         self.commandlist = []
         self.closing = False
+        self.ticks = 0
 
-    def loop(self):
+    def input_loop(self):
+        pass
+
+    def tick(self):
         pass
 
     def addcommand(self,name,min_argc,function):
@@ -46,3 +59,5 @@ class GameState:
         starttime = time.time()
         while self.closing != True and self.character.dead != True:
             time.sleep(TICK_SECONDS - ((time.time()) - starttime) % TICK_SECONDS)
+            self.ticks += 1
+            self.tick()
